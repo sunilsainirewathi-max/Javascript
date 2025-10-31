5e85a76b0d928c65e0395eae38f4f5396b39ef22
Sunil saini <sunilsaini.rewathi@gmail.com>
2025-10-31 05:39:45 +0000

    datatype

---
+++ javalearn/datatypes.js
@@ -0,0 +1,7 @@
+let name = "sunil khandela";
+let age = 24;
+let isStudent = true;
+let address = null;
+let phoneNumber;
+
+console.log(typeof null);         // string
---
+++ javalearn/script.js
@@ -0,0 +1,10 @@
+
+const fristName = "sunil khandela";
+let lastName = "saini";
+var mobileNumber = 8740927266;
+let testingVelue;
+
+
+
+
+console.table([fristName , lastName , mobileNumber,testingVelue]);
