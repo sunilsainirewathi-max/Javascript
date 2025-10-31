4ea67508e5eb13c4dd5f3609074bc75eced5acc6
Sunil saini <sunilsaini.rewathi@gmail.com>
2025-10-31 06:08:21 +0000

    conversition_data_type

--- javalearn/script.js
+++ javalearn/script.js
@@ -5,6 +5,11 @@ var mobileNumber = 8740927266;
 let testingVelue;
 
 
+let booleanValure = "khandela"; // true false 
+let booleanValureconert = Boolean(booleanValure); // convert string to boolean value 
+console.log(booleanValureconert); // print data-type
 
 
-console.table([fristName , lastName , mobileNumber,testingVelue]);
+
+//console.table([fristName , lastName , mobileNumber,testingVelue]);
+ 
